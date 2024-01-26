7e0b2c13e6efaf12f89bc3508931ee8b116d96ce
Emin Gürzoğlu <gurzoglum@gmail.com>
2024-01-26 19:19:58 +0300

    blabla

---
+++ MakeMeLaugh/Assets/Scripts/M_Scripts/M_IInteractable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface M_IInteractable
+{
+    public void Interact();
+    public string GetDescription();
+}
