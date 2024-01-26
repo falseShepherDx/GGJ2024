d13032676922793abfbdfd8f40f53b2caadf9e6a
Emin Gürzoğlu <gurzoglum@gmail.com>
2024-01-26 21:40:54 +0300

    Interaction System

--- MakeMeLaugh/Assets/Scripts/M_Scripts/M_PlayerInteract.cs
+++ MakeMeLaugh/Assets/Scripts/M_Scripts/M_PlayerInteract.cs
@@ -1,8 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class M_PlayerInteract : MonoBehaviour
 {
-    
+    public Transform InteractionSource;
+    public float InteractionRange;
+
+    public GameObject InteractionUI;
+    public TextMeshProUGUI interactionText;
+
+    private void Update()
+    {
+        InteractionRay();
+        
+    }
+
+    void InteractionRay()
+    {
+        Ray ray = new Ray(InteractionSource.position, InteractionSource.forward);
+        RaycastHit hit;
+        bool hitSomething = false;
+        if (Physics.Raycast(ray, out hit, InteractionRange))
+        {
+            hit.collider.gameObject.TryGetComponent(out M_IInteractable interactObj);
+            if (interactObj != null)
+            {
+                hitSomething = true;
+                interactionText.text = interactObj.GetDescription();
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                interactObj.Interact();
+                
+            }
+        }
+        InteractionUI.SetActive(hitSomething);
+    }
 }
