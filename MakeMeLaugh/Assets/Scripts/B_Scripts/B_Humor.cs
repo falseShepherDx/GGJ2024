22b6a045e696d774d4425ea2671f08eab1959c95
Barani-el <121324839+Barani-el@users.noreply.github.com>
2024-01-27 15:38:37 +0300

    Animasyonlar + gülme mekanik

--- MakeMeLaugh/Assets/Scripts/B_Scripts/B_Humor.cs
+++ MakeMeLaugh/Assets/Scripts/B_Scripts/B_Humor.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class B_Humor : MonoBehaviour
 {
-   
+    NavMeshAgent nvm;
+    public Animator anim;
+
     void Start()
     {
-        
+        nvm = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+            Humoric_Shot();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S");
+            Dead();
+        }
     }
 
     void Humoric_Shot()
     {
-     
+        nvm.speed = 0;
+        anim.SetBool("laugh" ,true);
     }
-
+    void Dead()
+    {
+        nvm.speed = 0;
+        anim.SetBool("dead", true);
+    }
     private void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.tag == this.gameObject.tag)
